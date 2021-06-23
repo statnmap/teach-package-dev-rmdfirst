d610b325830179219a16c375573d7334ad6d170b
statnmap <sebastien@thinkr.fr>
2021-06-23 16:39:48 +0200

    CI: test again renv

--- dev_history.R
+++ dev_history.R
@@ -15,7 +15,32 @@ renv::install("remotes")
 options(remotes.git_credentials = git2r::cred_user_pass("gitlab-ci-token", Sys.getenv("GITLAB_THINKR_TOKEN")))
 options(renv.auth.formation = list(GIT_PAT = Sys.getenv("GITLAB_THINKR_TOKEN")))
 renv::install("git::https://git.thinkr.fr/ThinkR/formation")
-remotes::install_git("https://git.thinkr.fr/ThinkR/formation", upgrade = FALSE)
+remotes::install_git("https://git.thinkr.fr/ThinkR/formation", upgrade = FALSE, git = "git2r")
+
+tmpform <- tempfile(pattern = "form-")
+git2r::clone("https://git.thinkr.fr/ThinkR/formation", local_path = tmpform,
+             credentials = git2r::cred_user_pass("gitlab-ci-token", Sys.getenv("GITLAB_THINKR_TOKEN")))
+remotes::install_local(path = tmpform, upgrade = FALSE)
+
+# Description
+# _Get all deps to run courses, except old ones -- SUGGESTS --
+all_courses <- list.files(here::here("courses"), full.names = TRUE, pattern = "[.]Rmd", recursive = TRUE)
+all_deps_courses <- attachment::att_from_rmds(all_courses)
+# _Get all deps to run templates_sc -- IMPORTS --
+all_compil_to_test <- "compile_course.R"
+# Extract all deps
+all_deps_compils <- attachment::att_from_rscripts(all_compil_to_test)
+
+supplements_imports <- c("git2r", "attachment", "testthat", "here", "flextable",
+                         "remotes")
+
+# Add to DESCRIPTION
+# usethis::use_description()
+attachment::att_to_desc_from_is(
+  imports = unique(c(supplements_imports, all_deps_courses)),
+  suggests = all_deps_compils
+)
+
 
 # Snapshot before push
 renv::snapshot()
